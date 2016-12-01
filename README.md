0ff354c27423ef03fd3b21bdd3cf658952dc0de1
SavageCore <talk@savagecore.eu>
2016-12-01 19:16:52 +0000

    0.1.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Userscript to generate a link to copy pthcamp command to clipboard
 
-![Imgur]()
+![Imgur](http://i.imgur.com/O2qq1A1.png)
 
 Click [here](https://github.com/SavageCore/pthcamp-link-creator/raw/master/src/pthcamp-link-creator.user.js) to install
---
+++ src/pthcamp-link-creator.user.js
@@ -0,0 +1,74 @@
+// ==UserScript==
+// @name         passtheheadphones.me - pthcamp Link Creator
+// @namespace    http://savagecore.eu/
+// @version      0.1.0
+// @description  Generate pthcamp command and copy to clipboard
+// @author       SavageCore
+
+// @include    http*://bandcamp.com/download?from=collection*
+
+// @downloadURL	 https://github.com/SavageCore/pthcamp-link-creator/raw/master/src/pthcamp-link-creator.user.js
+// @grant        GM_setClipboard
+
+// ==/UserScript==
+
+/*	global document, GM_setClipboard	*/
+
+(function () {
+	'use strict';
+	var pageData = document.getElementById('pagedata').getAttribute('data-blob');
+	pageData = JSON.parse(pageData);
+	var releaseDate = new Date(pageData.digital_items[0].release_date);
+	var downloadLinkElement = document.getElementsByClassName('download-title')[0];
+	var artist = pageData.digital_items[0].artist;
+	var title = pageData.digital_items[0].title;
+	var year = releaseDate.getFullYear();
+	var command = 'pthcamp "' + artist + '" "' + title + '" ' + year + ' ';
+	var linkElement;
+	var MutationObserver;
+
+	new MutationObserver(function (mutations) {
+		mutations.some(function (mutation) {
+			if (mutation.type === 'attributes' && mutation.attributeName === 'href') {
+				var downloadLink = mutation.target.href;
+				command += '"' + downloadLink + '"';
+				linkElement.setAttribute('style', 'color:#0687F5;cursor:pointer;display:inherit');
+				return true;
+			}
+
+			return false;
+		});
+	}).observe(downloadLinkElement, {
+		attributes: true,
+		attributeFilter: ['href'],
+		attributeOldValue: true,
+		characterData: false,
+		characterDataOldValue: false,
+		childList: false,
+		subtree: true
+	});
+
+	var target = document.getElementsByClassName('download')[0];
+
+	target.insertAdjacentHTML('afterend', '<p id="scCopyToClipboard">Copy pthcamp command</p>');
+
+	linkElement = document.getElementById('scCopyToClipboard');
+	linkElement.setAttribute('style', 'display: none;');
+
+	linkElement.onmouseover = function () {
+		linkElement.style['text-decoration'] = 'underline';
+	};
+
+	linkElement.onmouseout = function () {
+		linkElement.style['text-decoration'] = 'none';
+	};
+
+	linkElement.addEventListener('click', function () {
+		GM_setClipboard(command, 'text'); // eslint-disable-line new-cap
+		var originalText = linkElement.innerText;
+		linkElement.innerText = 'Copied!';
+		setTimeout(function () {
+			linkElement.innerText = originalText;
+		}, 2000);
+	}, false);
+})();
